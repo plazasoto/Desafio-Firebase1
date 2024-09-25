00e51991db2d0418a7b57f6669370d454634a4ac
plazasoto <jarrettsguitarsfactory@gmail.com>
2024-09-25 15:15:42 -0300

    Ingreso de datos y despliegue de lista

--- src/components/FormularioDatos.vue
+++ src/components/FormularioDatos.vue
@@ -1,5 +1,35 @@
 <template>
     <div>
         <h1>Formulario</h1>
+        <form @submit.prevent="addUsuario">
+            <input v-model="nombreUsuario" placeholder="Nombre Completo" />
+            <input v-model="correoUsuario" type="email" placeholder="Correo Electrónico">
+            <button type="submit">Agregar</button>
+        </form>
+
+
     </div>
 </template>
+
+<script>
+    import { getFirestore, collection, onSnapshot, addDoc, doc, deleteDoc } from 'firebase/firestore';
+    import firebaseApp from '../firebaseconfig';
+    export default {
+        data() {
+            return {
+                nombreUsuario: '',
+                correoUsuario: '',
+            };
+        },
+        methods: {
+            async addUsuario() {
+                if (this.nombreUsuario.trim() === '' ) return;
+                const db = getFirestore(firebaseApp);
+                const usersRef = collection(db, 'Usuarios' );
+                await addDoc(usersRef, { Nombre: this.nombreUsuario, Correo: this.correoUsuario });
+                this.nombreUsuario = ''; 
+                this.correoUsuario = ''; 
+            },
+        }
+    };
+</script>
--- src/components/ListaUsuarios.vue
+++ src/components/ListaUsuarios.vue
@@ -1,5 +1,30 @@
 <template>
     <div>
         <h2>Usuarios</h2>
+        <ul>
+            <li v-for="usuario in usuarios" :key="usuario.id">
+                {{ usuario.Nombre }} - {{ usuario.Correo }}
+            </li>
+        </ul>
     </div>
 </template>
+
+<script>
+    import { getFirestore, collection, onSnapshot, addDoc, doc, deleteDoc } from 'firebase/firestore';
+    import firebaseApp from '../firebaseConfig';
+
+    export default {
+        data() {
+            return {
+                usuarios: [],
+            };
+        },
+        mounted() {
+            const db = getFirestore(firebaseApp);
+            const usersRef = collection(db, 'Usuarios' );
+            onSnapshot(usersRef, (snapshot) => {
+                this.usuarios = snapshot.docs.map((doc) => ({ id: doc.id, ...doc.data() }));
+            });
+        },
+    };
+</script>
